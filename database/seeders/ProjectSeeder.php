25a47086c549847c60dbd7c03a1b06e92b2fc79e
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-03-13 23:18:09 +0100

    added randomly technologies to each project

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Str;
 use Illuminate\Support\Arr;
 use App\Models\Project;
 use App\Models\Type;
+use App\Models\Technology;
 use Faker\Generator as Faker;
 
 class ProjectSeeder extends Seeder
@@ -17,6 +18,8 @@ class ProjectSeeder extends Seeder
      */
     public function run(Faker $faker): void
     {
+        //getting technology ids
+        $technologies_ids = Technology::select('id')->pluck('id')->toArray();
 
         for($i=0; $i<10; $i++){
             $project = new Project();
@@ -25,6 +28,14 @@ class ProjectSeeder extends Seeder
             $project->type_id = rand(1, 4);
             $project->slug = Str::slug($project->title, '-');
             $project->save();
+
+            //after saving project in db, create randomly relations between projects and technologies
+            $project_technologies = [];
+            foreach($technologies_ids as $technology_id){
+                if(rand(0,1))$project_technologies[] = $technology_id;
+            }
+            $project->technologies()->attach($project_technologies);
+
         }
     }
 }
