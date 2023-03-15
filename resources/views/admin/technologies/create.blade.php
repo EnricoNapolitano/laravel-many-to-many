1c5df9a96405422c6ef234a7234444e45d5973f5
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-03-15 15:57:47 +0100

    create and edit view for technologies

---
+++ resources/views/admin/technologies/create.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+@section('content')
+
+<main class="container">
+    @include('includes.admin.form.technologies.form')
+</main>
+
+@endsection
---
+++ resources/views/admin/technologies/edit.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+<section id="technology-edit" class="container">
+    @include('includes.admin.form.technologies.form')
+</section>
+@endsection
