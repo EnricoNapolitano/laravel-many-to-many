34d9d12490cfbdc1597cf93d8d10dc8a829a9617
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-03-15 15:55:50 +0100

    created protected fillable for model Technology

--- app/Models/Technology.php
+++ app/Models/Technology.php
@@ -10,6 +10,9 @@ class Technology extends Model
 {
     use HasFactory;
 
+    //allow to fill 
+    protected $fillable = ['label', 'class_icon'];
+
     //relation with Project model
     public function projects(){
         return $this->belongsToMany(Project::class);
